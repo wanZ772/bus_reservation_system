333178cc1b47c5a5a57aa1b7cd68fec251940dda
Muhammad Najwan <najwanhassan03@gmail.com>
2024-06-11 00:05:14 +0800

    update

--- main2.c
+++ main2.c
@@ -1,15 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 char reserved_num[30] = {};
 char reserved_alph[30] = {};
-char customer_id[30][14] = {}, customer_name[30][50] = {}, customer_phone[30][12];
+char customer_id[30][14] = {}, customer_name[30][50] = {}, customer_phone[30][12] = {};
+float total_price = 0, base_price = 0;
+
+struct Destination  {
+    char *location;
+    float distance;
+};
+
+struct Destination destination[9] = {
+    {"Johor - Johor", 180.1801802},
+    {"Johor - Selangor", 342.1},
+    {"Johor - Kedah", 706.4},
+
+    {"Selangor - Kedah", 180.1801802},
+    {"Selangor - Johor", 342.1},
+    {"Selangor - Kedah", 371.6},
+
+    {"Kedah - Kedah", 180.1801802},
+    {"Kedah - Selangor", 371.6},
+    {"Kedah - Johor", 706.4},
+
+    };
+
+struct order_detail {
+    char *customer_name;
+    int customer_tracker;
+    char *customer_number;
+    char *customer_ic;
+};
+
+// struct order_detail customer_detail[30];
+
+
 int order_tracker = 0;
 int customer_tracker = 1;
 
 int reserved_checker(char target_alph, int target_id)   {
- ;
+ 
     for (int i = 0; i < order_tracker + 1; i++)    {
         if (target_alph == reserved_alph[i] && target_id == reserved_num[i])    {
             return 1;
@@ -44,20 +77,29 @@ void bus_diagram()  {
 int seat_reservation()  {
     char target_seat = 'D';
     int target_id = 0;    
-    printf("\nSelect seat: ");
-    scanf(" %1c%1d", &target_seat, &target_id);
-    system("cls");
-    printf("\nReserving seat: %c%d", target_seat, target_id);
+    int wanted_seat = 3;
+    printf("\nSeat wanted (1 - %d): ", 30-order_tracker);
+    scanf(" %d", &wanted_seat);
+    for (int i = 0; i < wanted_seat; i++)    {
+        printf("\nSelect seat (%d / %d): ", i+1, wanted_seat);
+        scanf(" %1c%2d", &target_seat, &target_id);    
 
-    int reserved_check = reserved_checker(target_seat, target_id);
-    if (reserved_check == 0) {
-        reserved_alph[order_tracker] = target_seat;
-        reserved_num[order_tracker] = target_id;
-        
-        return 1;
-    }   else    {
-        return 0;
+        if (reserved_checker(target_seat, target_id) == 0)  {
+            reserved_alph[order_tracker + i] = target_seat;
+            reserved_num[order_tracker + i] = target_id;
+            total_price += base_price;
+            // order_tracker++;
+        }   else    {
+            printf("\nSEAT RESERVED! PLEASE CHOOSE ANOTHER SEAT\n");
+            --i;
+            continue;
+        }
     }
+    system("cls");
+    printf("\nReserving seat:");
+        
+        return wanted_seat;
+ 
 }
 
 void show_customers()   {
@@ -72,22 +114,49 @@ void customer_list_update(char ic[14], char name[100], char phone[14]) {
     strcpy(customer_id[order_tracker], ic);
     strcpy(customer_name[order_tracker], name);
     strcpy(customer_phone[order_tracker], phone);
+
+    
+
+
+
     printf("\nYour name is: %s\n", name);
 }
 
-void payment()  {
+void select_destination()    {
+    // banner();
+    int depart, desti;
+    for (int i = 0; i < 9; i++) {
+        if (i % 3 == 0){printf("\n");}
+        printf("\n%d - %s", i+1, destination[i].location);
+    }
+    printf("\nDeparture - Destination: ");
+    scanf(" %d", &desti);
+    system("cls");
+    base_price = 3.33 * 2 * destination[desti-1].distance / 30;
+    printf("\nPrice per ticket: RM%.2f\n", base_price);
+
+    bus_diagram();
+}
+
+void payment(int seats)  {
     char name[100] = "", ic[14] = "", phone[14] = "";
-    // printf("Current tracker: %d\n", order_tracker);
+    printf("\n##################################\n");
+    printf("##\t      PAYMENT \t\t##\n");
+    printf("##\tTotal Price: %.2f \t##\n", total_price);
+    printf("##################################\n");
     printf("\nName: ");
-    scanf(" %s", name);
+    scanf(" %[^\n]%*c", name);
     printf("Ic Number: ");
     scanf("%s", ic);
     printf("Phone Number: ");
     scanf(" %s", phone);
     if (ic != "" || name != "" || phone != "")  {
+        for (int i = 0; i < seats; i++) {
+            customer_list_update(ic, name, phone);
+            order_tracker++;
+        }
         
-        customer_list_update(ic, name, phone);
-        order_tracker++;
+        
 
     } else  {
         printf("Invalid payment!");
@@ -101,7 +170,7 @@ void payment()  {
 
 
 void order_check(char target_id[12]) {
-    system("cls");
+    
     printf("\nDetails for customer: %s\n", target_id);
     for (int i = 0; i < 30; i++)    {
         if (strcmp(target_id, customer_id[i]) == 0)    {
@@ -114,31 +183,63 @@ void order_check(char target_id[12]) {
     }
 }
 
+void banner()   {
+    
+
+// printf(" ______                  ______                                                 _                     _                                     \n");
+// printf("(____  \\                (_____ \\                                          _    (_)                   | |                 _                 \n");
+// printf(" ____)  ) _   _   ___    _____) )  ____   ___   ____   ____  _   _  ____ | |_   _   ___   ____        \ \   _   _   ___ | |_   ____  ____   \n");
+// printf("|  __  ( | | | | /___)  (_____ (  / _  ) /___) / _  ) / ___)| | | |/ _  ||  _) | | / _ \ |  _ \        \ \ | | | | /___)|  _) / _  )|    \ \n");
+// printf("| |__)  )| |_| ||___ |        | |( (/ / |___ |( (/ / | |     \ V /( ( | || |__ | || |_| || | | |   _____) )| |_| ||___ || |__( (/ / | | | |\n");
+// printf("|______/  \\____|(___/        |_| \\____)(___/  \\____)|_|    \_/  \_||_| \___)|_| \___/ |_| |_|  (______/  \__  |(___/  \___)\____)|_|_|_|\n");
+// printf("                                                                                                            (____/                         \n");
+ 
+system("cls");
+printf(".-------------------------------------------------------------.\n");
+printf("'------..-------------..----------..----------..----------..--.|\n");
+printf("|       \\             ||          ||          ||          ||  ||\n");
+printf("|        \\            ||          ||          ||          ||  ||\n");
+printf("|    ..   ||  _    _  ||    _   _ || _    _   ||    _    _||  ||\n");
+printf("|    ||   || //   //  ||   //  // ||//   //   ||   //   //|| /||\n");
+printf("|_.------\"''----------''----------''----------''----------''--'|\n");
+printf(" |)|      |       |       |       |    |      mga|      ||==|  |\n");
+printf(" | |      |  _-_  |       |       |    |  .-.    |      ||==| C|\n");
+printf(" | |  __  |.'.-.' |   _   |   _   |    |.'.-.'.  |  __  | \"__=='\n");
+printf(" '---------'|( )|'----------------------'|( )|'----------\"\"     \n");
+printf("             '-'                          '-'\t");
+printf("Developed by Sinners");
+   
+}
+
+struct order_detail customer_detail[30];
+// customer_detail[0] = {"Najwan", 0, "010", "03"};
+
 void main()  {
-
+    
+    banner();
+    
     while (1){
-
+        
         int option;
-        printf("\n\nOption (1 - Select Seat / 2 - Exit / 3 - Reservation Check)>> ");
+        printf("\n\nOption (1 - Booking / 2 - Exit / 3 - Reservation Check)>> ");
         scanf(" %d", &option);
         if (option == 2)    {
+            banner();
             break;
         }   else if (option == 1)   {
-            system("cls");
+        
+            banner();
         }   else if (option == 3)   {
             char target_id[12];
             printf("Enter IC Number: ");
             scanf(" %s", target_id);
+            banner();
             order_check(target_id);
             continue;
         }
-        bus_diagram();
-        int reservation_result = seat_reservation();
-        if (reservation_result ==  1)   {
-            payment();
-        }   else    {
-            printf("\nSEAT RESERVATION FAILED! PLEASE TRY ANOTHER SEAT!\n");
-        }
-    }
-    
-}
+        select_destination();
+        int seats = seat_reservation();
+        
+            payment(seats);
+     
+    }}
